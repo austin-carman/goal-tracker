c016786ec2e118183636b34c9f96dd941233bb54
Austin Carman <abcarman12@gmail.com>
2021-05-29 11:40:44 -0600

    added input

--- src/components/AddGoal.js
+++ src/components/AddGoal.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import GoalList from './GoalList';
+
+const initialGoals = {
+    goal: '',
+}
 
 const AddGoal = () => {
     const { push } = useHistory();
+    const [ goals, setGoals ] = useState(initialGoals);
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setGoals({...goals, [name]:value});
+    };
 
     const handleSave = () => {
+        setGoals(initialGoals);
         push('/profile');
     };
 
     return(
         <div>
             <h2>Add Goal</h2>
+            <input
+                type='text'
+                name='goal'
+                value={goals.goal}
+                onChange={handleChange}
+            />
             <button onClick={handleSave}>Add</button>
         </div>
     );
